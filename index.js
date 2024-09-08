8902c75ec16c0e1a0cdcb8882e669e5328114004
Patel Divy <divypatel0403@gmail.com>
2024-09-08 11:41:15 +0530

    Added Redis controller

--- index.js
+++ index.js
@@ -1,23 +1,41 @@
 import express from "express";
-import dotenv from 'dotenv';
-import connectDb from "./config/database.js";
+import dotenv from "dotenv";
 import bodyParser from "body-parser";
-import authRoutes from './routes/auth.route.js';
-import quizRoutes from './routes/quiz.route.js';
+import authRoutes from "./routes/auth.route.js";
+import quizRoutes from "./routes/quiz.route.js";
+import connectAndLogRedisStatus from "./config/redisController.js";
+import connectDBandLogDB from "./config/database.js";
 
 const app = express();
 dotenv.config();
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+connectDBandLogDB()
+  .then((client) => {
+    console.log("Finding errors ");
+  })
+  .catch((error) => {
+    console.error("An error occurred in Database Operations :" + error);
+  });
 
-app.use('/api/auth', authRoutes);
-app.use('/api/quiz', quizRoutes);
+connectAndLogRedisStatus()
+  .then((client) => {
+    console.log("Performing Redis operations...");
+  })
+  .catch((error) => {
+    console.error(
+      "An error occurred while performing Redis operations:",
+      error
+    );
+  });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/api/auth", authRoutes);
+app.use("/api/quiz", quizRoutes);
 
 const port = process.env.PORT || 3000;
 
-connectDb();
-
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
